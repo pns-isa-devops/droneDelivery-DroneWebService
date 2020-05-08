a91bd142ee92b46db74e0964a6ce0c23a3e961f1
PaulK <merveille.koffi@etu.unice.fr>
2020-05-08 05:13:39 +0200

    #67 add delete commands

--- src/main/java/fr/unice/polytech/isa/dd/DroneWebService.java
+++ src/main/java/fr/unice/polytech/isa/dd/DroneWebService.java
@@ -2,6 +2,7 @@ package fr.unice.polytech.isa.dd;
 
 //import fr.unice.polytech.isa.dd
 
+import fr.unice.polytech.isa.dd.entities.DRONE_STATES;
 import fr.unice.polytech.isa.dd.entities.Drone;
 
 import javax.jws.WebMethod;
@@ -17,4 +18,11 @@ public interface DroneWebService {
     Boolean register(@WebParam(name="drone_id") String id, @WebParam(name="date") String date,
                            @WebParam(name="hour") String hour) throws ParseException;
 
+    @WebMethod
+    @WebResult(name = "delete_all")
+    boolean deleteAll();
+
+    @WebMethod
+    void changeStatus(@WebParam(name="status") DRONE_STATES states, @WebParam(name="drone") Drone drone, @WebParam(name="date") String date,
+                      @WebParam(name="hour") String hour) throws ParseException;
 }
--- src/main/java/fr/unice/polytech/isa/dd/DroneWebServiceImp.java
+++ src/main/java/fr/unice/polytech/isa/dd/DroneWebServiceImp.java
@@ -1,5 +1,6 @@
 package fr.unice.polytech.isa.dd;
 
+import fr.unice.polytech.isa.dd.entities.DRONE_STATES;
 import fr.unice.polytech.isa.dd.entities.Drone;
 
 import javax.ejb.EJB;
@@ -11,11 +12,28 @@ import java.text.ParseException;
 @Stateless(name = "DroneWS")
 public class DroneWebServiceImp implements DroneWebService {
 
-    @EJB private DroneRegister registry;
+    @EJB
+    private DroneRegister registry;
+    @EJB
+    private DroneStatusInterface st;
+
 
     @Override
-    public Boolean register(String id, String date,String hour) throws ParseException {
+    public Boolean register(String id, String date, String hour) throws ParseException {
         System.out.println("registerDrone");
-        return registry.register(id,date,hour);
+        return registry.register(id, date, hour);
     }
+
+    @Override
+    public boolean deleteAll() {
+        System.out.println("deleteAll");
+        return registry.deleteAll();
+    }
+
+    @Override
+    public void changeStatus(DRONE_STATES states, Drone drone, String date, String hour) throws ParseException {
+        System.out.println("changeDroneStatus");
+        st.changeStatus(states, drone, date, hour);
+    }
+
 }
